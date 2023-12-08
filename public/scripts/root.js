fa923dfd58514e7814af89d23161b3f2e13e842d
Alexey Kovzel <alexey.kovzel@gmail.com>
2023-12-08 14:56:54 +0100

    Improve naming

--- public/scripts/root.js
+++ public/scripts/root.js
@@ -10,7 +10,7 @@ window.uniqueId = function () {
 
 // Load scripts for header and footer
 const head = document.getElementsByTagName('head')[0];
-loadScript(head, '/scripts/components/footer.js');
+loadScript(head, 'scripts/components/footer.js');
 
 function loadScript(head, url, isModule = false) {
     let script = document.createElement('script');
