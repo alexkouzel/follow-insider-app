1342399608d6a21ae265bbfbf1204a80706d91f0
Alex Kouzel <aliaksei.kouzel@gmail.com>
2024-08-26 00:45:45 +0200

    Update Firebase hosting

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,6 +1,10 @@
 export class Config {
+
+    /* Production */
+    static serverUrl = 'follow-insider-eb-env.eba-hpznvsbn.eu-central-1.elasticbeanstalk.com ';
+
+    /* Development */
     static debugMode = location.hostname !== '127.0.0.1';
     static debugDelay = 500;
-    
-    static serverUrl = 'http://localhost:5000';
+
 }
